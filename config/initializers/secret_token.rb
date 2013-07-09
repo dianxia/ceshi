de1a9b055bb5e5cdf17e9b8082ae7bbfec4d9668
dianxia <834102823@qq.com>
2013-07-09 14:57:49 +0800

    ceshi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ceshi::Application.config.secret_token = '73e0d6453f31bdda24c5fb3ee36404e17c1c1b48d59739e6cf0701ac176028de1375c6edfcfb74ad93df4625e15b45432853b1c0ac68071b126d80dec1272397'
